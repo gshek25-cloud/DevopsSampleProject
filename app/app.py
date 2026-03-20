d1ff87c4c67b3d44a7cef45b3a3416c3ec8cd1c9
Sai Abhishek Gudiseva <gshek25@gmail.com>
2026-03-20 11:24:38 -0400

    Day13_Fix

--- app/app.py
+++ app/app.py
@@ -4,4 +4,3 @@ def greet():
 
 if __name__ == "__main__":
     print(greet())
-    
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,4 +3,3 @@ from app.app import greet
 
 def test_greet():
     assert greet() == "Hello from DevOps Project!"
-    
